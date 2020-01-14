1a97bec96e21270699c08793842d7e5ca8ce3a1e
MSoup <19704779+MSoup@users.noreply.github.com>
2020-01-14 12:26:26 +0900

    update name

--- turning_on_hues.py
+++ turning_on_hues.py
@@ -1,13 +1,17 @@
-#I heard that comments were important so here it is.
-
 import json
 import requests
 import time
+
+#Open file with key on my PC to get url to my hue bridge
 with open('C:\\Users\\charm\\OneDrive\\Desktop\\Christmas Turkey') as f:
     base_url = f.read().strip()
+
+#Turning off first to make sure it's running
 response = requests.put(base_url + 'lights/1/state', json.dumps({'on': False}))
 print(response.content)
+#Wait a second
 time.sleep(3)
+#Turn back on
 response = requests.put(base_url + 'lights/1/state', json.dumps({'on': True}))
 print(response.content)
 
